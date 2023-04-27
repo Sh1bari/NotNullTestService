620cd754e32a76d215a972cc79d544040fe78aa7
TheKelp1e <vova_krasnov_2004@mail.ru>
2023-04-27 17:39:54 +0300

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,30 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+
+import ru.sovcombank.hackaton.proto.ExchangeInfoMessage;
+import ru.sovcombank.hackaton.proto.MessageEnumsProto;
+import ru.sovcombank.hackaton.proto.OwnCommand;
+import ru.sovcombank.hackaton.proto.Request;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Socket socket = new Socket("localhost", 8888);
+        OutputStream out = socket.getOutputStream();
+        ExchangeInfoMessage exchangeInfoMessage = ExchangeInfoMessage.newBuilder()
+                .setRequest(Request.newBuilder()
+                        .setCommand(MessageEnumsProto.CommandType.ctHandshake)
+                        .setCommandForExec(OwnCommand.newBuilder()
+                                .setAlias("hello")
+                                .build())
+                        .build())
+                .build();
+        System.out.println(exchangeInfoMessage.getRequest().getCommand().name());
+        byte[] data = exchangeInfoMessage.toByteArray();
+        out.write(data);
+        out.flush();
+        Thread.sleep(9999999);
     }
 }
