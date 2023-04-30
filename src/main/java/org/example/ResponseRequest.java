4b17c4382102eae37f5d8fc5d90790e1afb6d96d
Sh1bari <vova_krasnov_2004@mail.ru>
2023-04-30 05:14:05 +0300

    test

--- src/main/java/org/example/ResponseRequest.java
+++ src/main/java/org/example/ResponseRequest.java
@@ -1,76 +1,14 @@
 package org.example;
 
-import com.google.protobuf.util.JsonFormat;
 import ru.sovcombank.hackaton.proto.*;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
 
-public class Main {
+public class ResponseRequest {
 
-    public static ExchangeInfoMessage exchangeInfoMessageHandshake = ExchangeInfoMessage.newBuilder()
-            .setHeader(Header.newBuilder()
-                    .setMessageNum("1")
-                    .setTimestamp(System.currentTimeMillis())
-                    .setSender("NotNullTestService")
-                    .setReceiver("NotNullServer")
-                    .build())
-
-            .setRequest(Request.newBuilder()
-                    .setCommand(MessageEnumsProto.CommandType.ctHandshake)
-                    .addSupportedCommands(OwnCommand.newBuilder()
-                            .setAlias("Идентификатор 1")
-                            .setCaption("Заголовок 1")
-                            .setDescription("Описание команды 1")
-                            .addParameters(Parameter.newBuilder()
-                                    .setAlias("Идентификатор команды 1")
-                                    .setCaption("Заголовок команды 1")
-                                    .setHint("Подсказка команды 1")
-                                    .setValue(ValueRef.newBuilder()
-                                            .setDataType(MessageEnumsProto.DataType.dtString)
-                                            .setFormat("формат данных")
-                                            .build())
-                                    .build())
-                            .addParameters(Parameter.newBuilder()
-                                    .setAlias("Идентификатор команды 2")
-                                    .setCaption("Заголовок 2 команды")
-                                    .setHint("Подсказка команды 2")
-                                    .setValue(ValueRef.newBuilder()
-                                            .setDataType(MessageEnumsProto.DataType.dtString)
-                                            .setFormat("формат данных")
-                                            .build())
-                                    .build())
-                            .build())
-                    .addSupportedCommands(OwnCommand.newBuilder()
-                            .setAlias("Идентификатор 2")
-                            .setCaption("Заголовок 2")
-                            .setDescription("Описание команды 2")
-                            .addParameters(Parameter.newBuilder()
-                                    .setAlias("Идентификатор команды 1")
-                                    .setCaption("Заголовок команды 1")
-                                    .setHint("Подсказка команды 1")
-                                    .setValue(ValueRef.newBuilder()
-                                            .setDataType(MessageEnumsProto.DataType.dtString)
-                                            .setFormat("формат данных")
-                                            .setValue("test value")
-                                            .build())
-                                    .build())
-                            .addParameters(Parameter.newBuilder()
-                                    .setAlias("Идентификатор команды 2")
-                                    .setCaption("Заголовок 2 команды")
-                                    .setHint("Подсказка команды 2")
-                                    .setValue(ValueRef.newBuilder()
-
-                                            .setDataType(MessageEnumsProto.DataType.dtString)
-                                            .setFormat("формат данных")
-                                            .build())
-                                    .build())
-                            .build())
-                    .build())
-            .build();
-
-    public static ExchangeInfoMessage exchangeInfoMessageStatus = ExchangeInfoMessage.newBuilder()
+    public static ExchangeInfoMessage exchangeInfoMessage = ExchangeInfoMessage.newBuilder()
             .setHeader(Header.newBuilder()
                     .setMessageNum("1")
                     .setTimestamp(System.currentTimeMillis())
@@ -340,15 +278,14 @@ public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         Socket socket = new Socket("localhost", 8888);
         OutputStream out = socket.getOutputStream();
-        byte[] data = exchangeInfoMessageHandshake.toByteArray();
-        out.write(data);
-        out.flush();
-        //Thread.sleep(10000);
+        ////////////////////////////////////////////////////////////////
 
-        byte[] dataStatus = exchangeInfoMessageStatus.toByteArray();
-        out.write(dataStatus);
+        //////////////////////////////////////////////////////////////
+        byte[] data = exchangeInfoMessage.toByteArray();
+        out.write(data);
         out.flush();
         Thread.sleep(10000);
         socket.close();
     }
+
 }
